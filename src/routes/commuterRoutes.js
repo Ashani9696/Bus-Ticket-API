8327c753c94f4fe741ba9f2bdef81fd41635ecef
Ashani Dilanka <ashani9996@gmail.com>
2024-12-26 22:08:35 +0530

    commuter functions

--- src/routes/commuterRoutes.js
+++ src/routes/commuterRoutes.js
@@ -9,5 +9,15 @@ router.use(protect, authorize('user'));
 router.get('/routes', commuterController.listRoutes);
 router.get('/routes/:routeId', commuterController.getRouteDetails);
 router.get('/trips/:tripId', commuterController.getTripDetails);
+router.get('/trips', commuterController.listTrips);
+
+router.post('/bookings', commuterController.makeBooking);
+router.get('/bookings/:bookingId', commuterController.getBookingDetails);
+router.put('/bookings/:bookingId', commuterController.modifyBooking);
+router.delete('/bookings/:bookingId', commuterController.cancelBooking);
+router.get('/bookings', commuterController.listBookings);
+
+router.post('/payments', commuterController.makePayment);
+
 
 module.exports = router;
